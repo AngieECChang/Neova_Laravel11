5f553d8ffb890438c489ac86f0e19368a8f4de8a
angie <angie.ecchang@gmail.com>
2025-03-24 18:40:27 +0800

    修改為Laravel 11 可用的版本

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+// import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: [
+        'resources/js/app.js',
+        'resources/css/app.css',
+        'resources/css/sb-admin-2.css',
+        'resources/css/sb-admin-2.min.css',
+      ],
+      refresh: true, // 支援熱重載
+    }),
+    // react(),
+  ],
 });
