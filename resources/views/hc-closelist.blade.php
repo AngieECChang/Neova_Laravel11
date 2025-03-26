d2d2da993375e79218d8580d8464c7b1f4cb5e17
angie <angie.ecchang@gmail.com>
2025-03-26 22:47:54 +0800

    取消 js 的引入，避免發生錯誤

--- resources/views/hc-closelist.blade.php
+++ resources/views/hc-closelist.blade.php
@@ -85,7 +85,6 @@
 </div>
 @include('includes.reopen-case')
 @include('includes.delete-close')
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
   $(document).ready(function() {
     $("#reopenDate").datepicker({
