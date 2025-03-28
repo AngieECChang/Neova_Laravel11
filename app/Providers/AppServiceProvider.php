57ca412d619d14db8d4f39b09ca8dd2d809e8eee
angie <angie.ecchang@gmail.com>
2025-03-28 12:26:03 +0800

    增加依據身分證取得所有在職員工以及有選擇的離職員工，可以給所有blade使用

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,11 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
 use App\Http\Controllers\AddressController;
+use App\Services\DatabaseConnectionService;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -31,5 +35,34 @@ class AppServiceProvider extends ServiceProvider
 	{
 		// 註冊 API 路由（讓 Blade 前端能 AJAX 使用）
 		Route::middleware('web')->get('/api/towns', [AddressController::class, 'getTowns']);
+
+		View::composer('*', function ($view) {
+			$selected_id = request()->input('employeeID');
+			$databaseName = session('DB');
+			$db = DatabaseConnectionService::setConnection($databaseName);
+
+			$employees  = $db->table('employees')
+      ->whereNotNull('IdNo')
+      ->where('IdNo', '!=', '')
+      ->where('status', 1)
+      ->orderBy('employeeNo')
+      ->orderBy('employeeID')
+      ->get();
+
+			// 檢查選取的 employee 是否是離職的
+			$selected_quit = null;
+			if ($selected_id) {
+				$selected_quit = $db->table('employees')
+					->where('employeeID', $selected_id)
+					->where('status', 0)
+					->whereNotNull('IdNo')
+					->where('IdNo', '!=', '')
+					->first();
+			}
+
+			View::share('employeesListByIdNo', $employees);
+			View::share('selected_employeesByIdNo_quit', $selected_quit);
+			View::share('selected_employeesByIdNo_id', $selected_id);
+		});
 	}
 }
