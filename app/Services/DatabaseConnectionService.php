f47d733bdb66ae6cf02444be06acd1d8b4a73095
angie <angie.ecchang@gmail.com>
2025-03-31 16:01:02 +0800

    解決_每次切都會動態新增 config，長時間可能累積過多 config

--- app/Services/DatabaseConnectionService.php
+++ app/Services/DatabaseConnectionService.php
@@ -6,12 +6,18 @@ use Illuminate\Support\Facades\DB;
 
 class DatabaseConnectionService
 {
+    private static $initializedConnections = [];
+
     public static function setConnection($databaseName, $port = 3306)
     {
         $connectionName = $databaseName; // 確保名稱唯一
         $host = '127.0.0.1';
         $username = 'neova';
         $password = 'neova@1234';
+
+        if (in_array($connectionName, self::$initializedConnections)) {
+            return DB::connection($connectionName);
+        }
         // 檢查是否已經建立該連線，避免重複建立
         if (!array_key_exists($connectionName, Config::get('database.connections'))) {
             Config::set("database.connections.$connectionName", [
@@ -22,13 +28,14 @@ class DatabaseConnectionService
               'username'  => $username,
               'password'  => $password,
               'charset'   => 'utf8mb4',
-              'collation' => 'utf8mb4_unicode_ci',
+              'collation' => 'utf8mb4_general_ci',
               'prefix'    => '',
               'strict'    => false,
               'engine'    => null,
             ]);
         }
-
+        self::$initializedConnections[] = $connectionName;
+        
         return DB::connection($connectionName);
     }
 }
