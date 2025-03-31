90d9ca9984a38498026a2741c311cad30236348d
angie <angie.ecchang@gmail.com>
2025-03-31 23:01:23 +0800

    增加左邊選單手風琴效果（accordion）_「點擊某一個分類 (cate_shortname) 時，其它已展開的分類收合起來」

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,12 +35,13 @@
         </div>
       </div>
       </li>
+      <div id="accordionCategories">
       @foreach ($allow_permission as $cateID => $permission)
         <hr class="sidebar-divider">
         <a href="#" class="sidebar-heading toggle-category" data-toggle="collapse" data-target="#category{{$cateID}}" aria-expanded="false" aria-controls="category{{$cateID}}" style="color:#f6c23e;font-size:12pt !important;font-weight: 600 !important; text-decoration: none; padding-bottom:10px;">
             {{ $permission->first()->cate_shortname }}
         </a>
-        <div id="category{{$cateID}}" class="collapse">
+        <div id="category{{$cateID}}" class="collapse" data-parent="#accordionCategories">
           @php
             $subCategories = collect($permission)->groupBy('subcate_name');
           @endphp
@@ -66,93 +67,11 @@
           @endforeach
         </div>
       @endforeach
-
-      <hr class="sidebar-divider">
-      <!-- Heading -->
-      <div class="sidebar-heading">
-        Interface
       </div>
-      <!-- Nav Item - Pages Collapse Menu -->
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-          aria-expanded="true" aria-controls="collapseTwo">
-          <i class="fas fa-fw fa-cog"></i>
-          <span>Components</span>
-        </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-          <div class="bg-white py-2 collapse-inner rounded">
-              <h6 class="collapse-header">Custom Components:</h6>
-              <a class="collapse-item" href="buttons.html">Buttons</a>
-              <a class="collapse-item" href="cards.html">Cards</a>
-          </div>
-        </div>
-      </li>
-      <!-- Nav Item - Utilities Collapse Menu -->
-      <li class="nav-item">
-          <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-              aria-expanded="true" aria-controls="collapseUtilities">
-              <i class="fas fa-fw fa-wrench"></i>
-              <span>Utilities</span>
-          </a>
-          <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
-              data-parent="#accordionSidebar">
-              <div class="bg-white py-2 collapse-inner rounded">
-                  <h6 class="collapse-header">Custom Utilities:</h6>
-                  <a class="collapse-item" href="utilities-color.html">Colors</a>
-                  <a class="collapse-item" href="utilities-border.html">Borders</a>
-                  <a class="collapse-item" href="utilities-animation.html">Animations</a>
-                  <a class="collapse-item" href="utilities-other.html">Other</a>
-              </div>
-          </div>
-      </li>
-      <hr class="sidebar-divider">
-      <!-- Heading -->
-      <div class="sidebar-heading">
-          Addons
-      </div>
-
-      <!-- Nav Item - Pages Collapse Menu -->
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
-          aria-expanded="true" aria-controls="collapsePages">
-          <i class="fas fa-fw fa-folder"></i>
-          <span>Pages</span>
-        </a>
-        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-          <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Login Screens:</h6>
-            <a class="collapse-item" href="login.html">Login</a>
-            <a class="collapse-item" href="register.html">Register</a>
-            <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-            <div class="collapse-divider"></div>
-            <h6 class="collapse-header">Other Pages:</h6>
-            <a class="collapse-item" href="404.html">404 Page</a>
-            <a class="collapse-item" href="blank.html">Blank Page</a>
-          </div>
-        </div>
-      </li>
-      <!-- Nav Item - Charts -->
-      <li class="nav-item">
-        <a class="nav-link" href="charts.html">
-          <i class="fas fa-fw fa-chart-area"></i>
-          <span>Charts</span></a>
-      </li>
-      <!-- Nav Item - Tables -->
-      <li class="nav-item">
-        <a class="nav-link" href="tables.html">
-          <i class="fas fa-fw fa-table"></i>
-          <span>Tables</span></a>
-      </li>
       <hr class="sidebar-divider d-none d-md-block">
       <!-- Sidebar Toggler (Sidebar) -->
       <div class="text-center d-none d-md-inline">
           <button class="rounded-circle border-0" id="sidebarToggle"></button>
-      </div>
-      <!-- Sidebar Message -->
-      <div class="sidebar-card d-none d-lg-flex">
-          <img class="sidebar-card-illustration mb-2" src="img/undraw_rocket.svg" alt="...">
-          <p class="text-center mb-2"><strong>SB Admin Pro</strong> is packed with premium features, components, and more!</p>
-          <a class="btn btn-success btn-sm" href="https://startbootstrap.com/theme/sb-admin-pro">Upgrade to Pro!</a>
       </div>
     </ul>
     <!-- End of Sidebar -->
