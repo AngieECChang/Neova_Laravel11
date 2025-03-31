4bcfadfd2f7a29635c84705b9abf124639918db5
angie <angie.ecchang@gmail.com>
2025-03-31 16:24:15 +0800

    增加 Vite 使用

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,18 @@ import laravel from 'laravel-vite-plugin';
 // import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  server: {
+    host: 'laravel.local',
+    port: 5173,
+    strictPort: true,
+    cors: true, //關鍵設定：允許跨來源
+    origin: 'http://laravel.local:5173',
+    hmr: {
+      protocol: 'ws',
+      host: 'laravel.local',
+      port: 5173,
+    },
+  },
   plugins: [
     laravel({
       input: [
@@ -10,6 +22,7 @@ export default defineConfig({
         'resources/css/app.css',
         'resources/css/sb-admin-2.css',
         'resources/css/sb-admin-2.min.css',
+        'resources/js/hcevaluation/hcevaluation01_medical.js',
       ],
       refresh: true, // 支援熱重載
     }),
